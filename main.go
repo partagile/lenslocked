a923c3635af447d0837633e2f7fa7fbd060fb2e1
partagile <hello@thushyanthan.com>
2023-06-30 19:39:18 -0700

    using chi router and tidying up code

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +19,14 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>FAQ page</h1>")
+	fmt.Fprint(w, `<h1>FAQ page</h1>
+	this is a multi-line string <br />
+	gonna add some line breaks <br />
+	<ul>
+	<li><strong>Question 1</strong> - Yup!</li>
+	<li><strong>Question 2</strong> - Not so fast</li>
+	<li><strong>Question 3</strong> - Satisfaction guaranteed!</li>
+	`)
 }
 
 func pathHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,14 +39,22 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 		faqHandler(w, r)
 	default:
 		// notFoundHandler(w, r) // returns 200 which is not an appropriate status
-		http.Error(w, "Doh! 404 Not Found!", http.StatusNotFound)
+
 	}
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/", pathHandler)
 	// http.HandleFunc("/contact", contactHandler)
 	// http.HandleFunc("/faq", faqHandler)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/faq", faqHandler)
+	r.Get("/contact", contactHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Doh! 404 Not Found!", http.StatusNotFound)
+	})
+
 	fmt.Println("Starting the server on :3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }

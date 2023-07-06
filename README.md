75a06514b81aa8cb6b450f80a945813dcbc49ea2
partagile <hello@thushyanthan.com>
2023-07-05 21:43:39 -0700

    connect to postgres using pgx in cmd-exp

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # lenslocked
 This is a photo gallery application written in Go. I'm following the course material at [usegolang.com](https://www.usegolang.com/).
+
+# On Windows
+- Development is done on a Windows machine
+- Packages installed via chocolatey
+
+## Packages
+- `choco install golang`
+- `choco install docker-desktop`
+
+## docker
+- postgres
+- adminer
--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,42 +1,45 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-type User struct {
-	Name string
-	Age  int
-	Meta UserMeta
+type PostgresConfig struct {
+	Host string
+	Port string
+	User string
+	Password string
+	DBName string
+	SSLMode string
 }
 
-type UserMeta struct {
-	Visits int
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode)
 }
 
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	cfg := PostgresConfig{
+		Host: "localhost",
+		Port: "5432",
+		User: "bhaloo",
+		Password: "junglebook",
+		DBName: "lenslocked",
+		SSLMode: "disable",
+	}
+	
+	db, err := sql.Open("pgx", cfg.String())
+
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
 
-	// user := struct {
-	// 	Name string
-	// }{
-	// 	Name: "John Smith",
-	// }
-
-	user := User{
-		Name: "John Smith",
-		Age:  28,
-		Meta: UserMeta{
-			Visits: 3,
-		},
-	}
-
-	err = t.Execute(os.Stdout, user)
+	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Connected!")
 }

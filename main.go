3d632c06dcb3e3669ebb5e5c5995242daeecea00
partagile <hello@thushyanthan.com>
2023-07-07 13:18:38 -0700

    creating signin page and connecting it up

--- main.go
+++ main.go
@@ -42,8 +42,11 @@ func main() {
 		UserService: &userService,
 	}
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
+
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
 
 	r.Get("/chi/{userID}", chiRequestUrlParamHandler)
 

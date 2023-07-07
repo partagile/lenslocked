eee92c4985b0c3f47374d6d6435bb39747a7166f
partagile <hello@thushyanthan.com>
2023-07-07 12:43:13 -0700

    adding postgres to models

---
+++ models/postgres.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+// Open will open a SQL connection with the provided
+// Postgres database. If you call this, you will need
+// to ensure connection is closed wigh db.Close() method.
+func Open(config PostgresConfig) (*sql.DB, error) {
+	db, err := sql.Open("pgx", config.String())
+
+	if err != nil {
+		return nil, fmt.Errorf("open %w", err)
+	}
+	return db, nil
+}
+
+func DefaultPostgresConfig() PostgresConfig {
+	return PostgresConfig{
+		Host: "localhost",
+		Port: "5432",
+		User: "bhaloo",
+		Password: "junglebook",
+		DBName: "lenslocked",
+		SSLMode: "disable",
+	}
+}
+
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode)
+}

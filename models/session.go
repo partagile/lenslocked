8af10365625edea5d28e8809c987463d95d88478
partagile <hello@thushyanthan.com>
2023-07-09 06:49:14 -0700

    stubbing the session service

--- models/session.go
+++ models/session.go
@@ -1,7 +1,28 @@
 package models
 
+import "database/sql"
+
 type Session struct {
-	ID        int
-	UserID    int
+	ID     int
+	UserID int
+	// Token is set when creating a new session.
+	// This is left empty when looking up an existing session since
+	// only the hash is stored
+	Token     string
 	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+func (ss *SessionService) Create(userID int) (*Session, error) {
+	// TODO: create session token
+	// TODO: implement SessionService.Create
+	return nil, nil
+}
+
+func (ss *SessionService) User(token string) (*User, error) {
+	// TODO: Implement SessionService.User
+	return nil, nil
 }

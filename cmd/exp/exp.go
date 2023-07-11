e977fde700ee354a0a73fe6bd90c8f02fec104f8
partagile <hello@thushyanthan.com>
2023-07-11 08:49:16 -0700

    asserting type

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 )
 
 // TL;DR - don't export keys nor the type that's being used for context
@@ -21,19 +22,19 @@ func main() {
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, "favorite-color", "blue")
 	value := ctx.Value("favorite-color")
-	fmt.Println(value)
+	fmt.Println("Value from 'favorite-color':", value)
 
 	// value will now return nil
 	// "favoriteColor" is NOT the same type as the "favorite-key" above
 	ctx = context.WithValue(ctx, "favorite-color", "blue")
 	value = ctx.Value(favoriteColor)
-	fmt.Println(value)
+	fmt.Println("Value from ctxKey:", value)
 
 	// value will now return the correct value
 	// ctx was set with "favoriteColor" and value pulls it out
 	ctx = context.WithValue(ctx, favoriteColor, "blue")
 	value = ctx.Value(favoriteColor)
-	fmt.Println(value)
+	fmt.Println("Value from correct ctxKey:", value)
 
 	// value1 and value2 ensures that even if other libraries
 	// inadvertently set "favorite-color", the custom context
@@ -41,5 +42,24 @@ func main() {
 	ctx = context.WithValue(ctx, "favorite-color", 123)
 	value1 := ctx.Value("favorite-color")
 	value2 := ctx.Value(favoriteColor)
-	fmt.Println(value1, value2)
+	fmt.Printf("---\nValue1 (int): %d\nValue2 (str): %s\n", value1, value2)
+
+	// Using type assertions
+	strValue, ok := value2.(string)
+	if !ok {
+		fmt.Println("not cool bro")
+	}
+	fmt.Printf("---\nAsserted value: %s\n", strValue)
+	fmt.Println("Having prefix value (str)", strings.HasPrefix(strValue, "b"))
+
+	// type assert to int - will fail
+	fmt.Println("\n--- typing to int and checking 'ok'")
+	value = ctx.Value(favoriteColor)
+	intValue, ok := value.(int)
+	if !ok {
+		fmt.Println("it's not an int, bro")
+		return
+	}
+	fmt.Printf("---\nAsserted value: %q\n", intValue)
+	//fmt.Println("Having prefix value (str)", strings.HasPrefix(strValue, "b"))
 }

25bccab494d5fbb8629568de2e6c2105476f57ee
partagile <hello@thushyanthan.com>
2023-07-12 09:47:43 -0700

    adding .env file and loading config from env

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"strings"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 )
 
 // TL;DR - don't export keys nor the type that's being used for context
@@ -17,49 +19,19 @@ const (
 )
 
 func main() {
-	//Exploring contexts
-	// using string as a key
-	ctx := context.Background()
-	ctx = context.WithValue(ctx, "favorite-color", "blue")
-	value := ctx.Value("favorite-color")
-	fmt.Println("Value from 'favorite-color':", value)
-
-	// value will now return nil
-	// "favoriteColor" is NOT the same type as the "favorite-key" above
-	ctx = context.WithValue(ctx, "favorite-color", "blue")
-	value = ctx.Value(favoriteColor)
-	fmt.Println("Value from ctxKey:", value)
-
-	// value will now return the correct value
-	// ctx was set with "favoriteColor" and value pulls it out
-	ctx = context.WithValue(ctx, favoriteColor, "blue")
-	value = ctx.Value(favoriteColor)
-	fmt.Println("Value from correct ctxKey:", value)
-
-	// value1 and value2 ensures that even if other libraries
-	// inadvertently set "favorite-color", the custom context
-	// "favoriteColor" is under your/the dev's control
-	ctx = context.WithValue(ctx, "favorite-color", 123)
-	value1 := ctx.Value("favorite-color")
-	value2 := ctx.Value(favoriteColor)
-	fmt.Printf("---\nValue1 (int): %d\nValue2 (str): %s\n", value1, value2)
-
-	// Using type assertions
-	strValue, ok := value2.(string)
-	if !ok {
-		fmt.Println("not cool bro")
+	// Read config info from .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
-	fmt.Printf("---\nAsserted value: %s\n", strValue)
-	fmt.Println("Having prefix value (str)", strings.HasPrefix(strValue, "b"))
 
-	// type assert to int - will fail
-	fmt.Println("\n--- typing to int and checking 'ok'")
-	value = ctx.Value(favoriteColor)
-	intValue, ok := value.(int)
-	if !ok {
-		fmt.Println("it's not an int, bro")
-		return
+	host := os.Getenv("SMTP_HOST")
+	portStr := os.Getenv("SMTP_PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		panic(err)
 	}
-	fmt.Printf("---\nAsserted value: %q\n", intValue)
-	//fmt.Println("Having prefix value (str)", strings.HasPrefix(strValue, "b"))
+	username := os.Getenv("SMTP_USERNAME")
+	password := os.Getenv("SMTP_PASSWORD")
+
 }

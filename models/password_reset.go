21010fa3d70e1da230d228c84bd675e1625143cf
partagile <hello@thushyanthan.com>
2023-07-13 20:05:36 -0700

    adding PasswordResetService.Consume. yay!

--- models/password_reset.go
+++ models/password_reset.go
@@ -84,10 +84,47 @@ func (service *PasswordResetService) Create(email string) (*PasswordReset, error
 }
 
 func (service *PasswordResetService) Consume(token string) (*User, error) {
-	return nil, fmt.Errorf("TODO: Implement PasswordResetService.Consume")
+	// 1. validate token; ensure it is not expired
+	tokenHash := service.hash(token)
+	var user User
+	var pwReset PasswordReset
+	row := service.DB.QueryRow(`
+		SELECT password_resets.id, 
+			password_resets.expires_at,
+			users.id, 
+			users.email, 
+			users.password_hash
+		FROM password_resets
+			JOIN users on users.id = password_resets.user_id
+		WHERE password_resets.token_hash = $1;`, tokenHash)
+	err := row.Scan(&pwReset.ID, &pwReset.ExpiresAt,
+		&user.ID, &user.Email, &user.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("pw reset consume %w:", err)
+	}
+	if time.Now().After(pwReset.ExpiresAt) {
+		return nil, fmt.Errorf("token expiration %v", token)
+	}
+	// 2. ensure we have the userid for the token
+	// 3. delete the token once consumed
+	err = service.delete(pwReset.ID)
+	if err != nil {
+		return nil, fmt.Errorf("consume can't delete %w", err)
+	}
+	return &user, nil
 }
 
 func (service *PasswordResetService) hash(token string) string {
 	tokenHash := sha256.Sum256([]byte(token))
 	return base64.URLEncoding.EncodeToString(tokenHash[:])
+}
+
+func (service *PasswordResetService) delete(id int) error {
+	_, err := service.DB.Exec(`
+		DELETE FROM password_resets
+		WHERE id = $1;`, id)
+	if err != nil {
+		return fmt.Errorf("failed to delete reset token %w", err)
+	}
+	return nil
 }

991228ae8f3fe64f4ac4ba99f194b0f4c893a064
partagile <hello@thushyanthan.com>
2023-07-14 09:19:17 -0700

    using pgconn and pgerrcode for better err msgs

--- models/user.go
+++ models/user.go
@@ -2,10 +2,17 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"strings"
 
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgerrcode"
 	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrEmailTaken = errors.New("models: email address already in use")
 )
 
 type User struct {
@@ -35,6 +42,12 @@ func (us *UserService) Create(email, password string) (*User, error) {
 		VALUES ($1, $2) RETURNING id;`, email, passwordHash)
 	err = row.Scan(&user.ID)
 	if err != nil {
+		var pgError *pgconn.PgError
+		if errors.As(err, &pgError) {
+			if pgError.Code == pgerrcode.UniqueViolation {
+				return nil, ErrEmailTaken
+			}
+		}
 		return nil, fmt.Errorf("Create user: %w", err)
 	}
 	return &user, nil

066b426cbfa76e35c182aaf786f9efafa34946e9
partagile <hello@thushyanthan.com>
2023-07-16 16:31:40 -0700

    add gallery update handler

--- main.go
+++ main.go
@@ -145,6 +145,7 @@ func main() {
 			r.Get("/new", galleriesC.New)
 			r.Post("/", galleriesC.Create)
 			r.Get("/{id}/edit", galleriesC.Edit)
+			r.Post("/{id}", galleriesC.Update)
 		})
 	})
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {

073d2a98cc182343481d771043148cfbaa31b2c8
partagile <hello@thushyanthan.com>
2023-07-18 09:31:41 -0700

    upload files and redirect to edit page

--- models/gallery.go
+++ models/gallery.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -142,6 +143,25 @@ func (service *GalleryService) Image(galleryID int, filename string) (Image, err
 		GalleryID: galleryID,
 		Path:      imagePath,
 	}, nil
+}
+
+func (service *GalleryService) CreateImage(GalleryID int, filename string, contents io.Reader) error {
+	galleryDir := service.galleryDir(GalleryID)
+	err := os.MkdirAll(galleryDir, 0755)
+	if err != nil {
+		return fmt.Errorf("creating gallery-%d image dir: %w", GalleryID, err)
+	}
+	imagePath := filepath.Join(galleryDir, filename)
+	dst, err := os.Create(imagePath)
+	if err != nil {
+		return fmt.Errorf("creating image file: %w", err)
+	}
+	defer dst.Close()
+	_, err = io.Copy(dst, contents)
+	if err != nil {
+		return fmt.Errorf("copying contents to image file: %w", err)
+	}
+	return nil
 }
 
 func (service *GalleryService) DeleteImage(galleryID int, filename string) error {
